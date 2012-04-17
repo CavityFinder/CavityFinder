803e2356f3ac0622ece43fd87e41c476ada7b4a5
john <john@john-ACER>
2012-04-16 21:16:04 -0400

    Added input argument parsing.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
-
 public class Main {
-
+	public static void main(String[] args) {
+		//for testing
+		args =new String[]{"-i","~/Desktop/input.pdb","-o","~/Desktop/output.pdb","-probe","1.7","-resolution","0.25"};
+		String usageString = "Usage: mycode -i inputfile -o outputfile -probe 1.7 -resolution 0.25";
+		String inputFilePath = null;
+		String outputFilePath = null;
+		double probeSize = 0;
+		double resolution = 0;
+		if(args.length!=8)
+			System.out.println(usageString);
+		for (int i = 0; i < 8; i += 2) {
+			if (args[i].equals("-i")) {
+				inputFilePath = args[i+1];
+			} else if (args[i].equals("-o")) {
+				outputFilePath = args[i+1];
+			} else if (args[i].equals("-probe")) {
+				probeSize = Double.parseDouble(args[i+1]);
+			} else if (args[i].equals("-resolution")) {
+				resolution = Double.parseDouble(args[i+1]);
+			} else {
+				System.out.println(usageString);
+			}
+		}
+		System.out.println(inputFilePath+" "+outputFilePath+" "+probeSize+" "+resolution);
+	}
 }
