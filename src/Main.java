b38dfd05955a4844fa0206ac64f576a2c05d0578
john <john@john-ACER>
2012-04-19 14:18:42 -0400

    slight refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+import java.io.File;
+
 public class Main {
 	public static void main(String[] args) {
 		//for testing
+		//remove this line when building
 		args =new String[]{"-i","~/Desktop/input.pdb","-o","~/Desktop/output.pdb","-probe","1.7","-resolution","0.25"};
-		String usageString = "Usage: mycode -i inputfile -o outputfile -probe 1.7 -resolution 0.25";
 		String inputFilePath = null;
 		String outputFilePath = null;
 		double probeSize = 0;
 		double resolution = 0;
 		if(args.length!=8)
-			System.out.println(usageString);
+			inputError();
 		for (int i = 0; i < 8; i += 2) {
 			if (args[i].equals("-i")) {
 				inputFilePath = args[i+1];
@@ -19,9 +21,15 @@ public class Main {
 			} else if (args[i].equals("-resolution")) {
 				resolution = Double.parseDouble(args[i+1]);
 			} else {
-				System.out.println(usageString);
+				inputError();
 			}
 		}
 		System.out.println(inputFilePath+" "+outputFilePath+" "+probeSize+" "+resolution);
+		File input = new File(inputFilePath);
+		
+	}
+	public static void inputError(){
+		System.out.println("Usage: mycode -i inputfile -o outputfile -probe 1.7 -resolution 0.25");
+		System.exit(1);
 	}
 }
