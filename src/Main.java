ff0fba16ea69855668fe2700ff45ed20336e4578
john <john@mooring.com>
2012-04-20 16:19:08 -0400

    Created Universe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,12 +11,10 @@ public class Main {
 		args = new String[] { "-i", "~/Desktop/input.pdb", "-o",
 				"~/Desktop/output.pdb", "-probe", "1.7", "-resolution", "0.25" };
 		ArrayList<Atom> atoms = new ArrayList<Atom>();
-		byte[][][] atomArray;
 		String inputFilePath = null;
 		String outputFilePath = null;
 		double probeSize = 0;
 		double resolution = 0;
-		double minX, maxX, minY, maxY, minZ, maxZ;
 		if (args.length != 8)
 			inputError();
 		for (int i = 0; i < 8; i += 2) {
@@ -52,11 +51,28 @@ public class Main {
 	public static void eraseProbeAtoms(){
 		
 	}
+	
+	public static void printOutputFile(String outputFile, ArrayList<Atom> loadedAtoms, byte[][][]probeAtoms){
+		try {
+			PrintWriter out = new PrintWriter(new File(outputFile));
+			for(Atom atom:loadedAtoms)
+				out.println(atom);
+			for(int i = 0; i<probeAtoms.length;i++){
+				for(int j = 0; j<probeAtoms[0].length;j++){
+					for(int k = 0; k<probeAtoms[0][0].length;k++){
+						
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error printing output file.");
+		}
+	}
 
 	public static void eraseNonCavities(byte[][][] atoms, int cx, int cy, int cz) {
-		if (atoms[cx][cy][cz] == 0)
+		if (atoms[cx][cy][cz] ==-1)
 			return;
-		atoms[cx][cy][cz]=1;
+		atoms[cx][cy][cz]= -1;
 		for (int i = cx - 1; i <= cx + 1; i++) {
 			for (int j = cy - 1; j < cy + 1; j++) {
 				for (int k = cz - 1; k < cz + 1; k++) {
